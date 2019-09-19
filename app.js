98c76b5b56ccf50347afdce0d10ff2f7b743ec97
Dave Song <hyunwoo@Daves-Air.fios-router.home>
2019-09-18 23:04:37 -0400

    updated files

---
+++ app.js
@@ -0,0 +1,9 @@
+//Click home to get to home
+
+
+//Click projects scroll down to projects section
+
+//Click contact for contact page
+//Contact page should have email, github profile, linkedin page
+
+//Click hamburger icon that brings drop down of home, projects, contact
