39e61c4914a181b24563ba05b5e141aad8d2ccde
Dave Song <hyunwoo@Daves-Air.fios-router.home>
2019-09-19 23:20:04 -0400

    finalized portfolio

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-//Click home to get to home
-
-
 //Click projects scroll down to projects section
+$(".list-of-projects").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#quiz-app").offset().top
+    }, 1000);
+});
 
-//Click contact for contact page
-//Contact page should have email, github profile, linkedin page
-
-//Click hamburger icon that brings drop down of home, projects, contact
+$(".bio").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".about-me-section").offset().top
+    }, 1000);
+});
